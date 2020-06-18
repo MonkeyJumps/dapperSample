b8c82e4603d924410f97ce01e04213e8aa82f4e3
michael vasquez <michael.vasquez@psigen.com>
2020-06-17 18:56:01 -0700

    initial commit, but Data access layer set up using dapper. Also simple asp.net core mvc site set with simple form for sign up of new users

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DapperWebApp.Models;
+using DataLayer.BusinessLogic;
+
+namespace DapperWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeProcessor _employeeProcessor;
+
+        public HomeController(ILogger<HomeController> logger, IEmployeeProcessor employeeProcessor)
+        {
+            _logger = logger;
+            _employeeProcessor = employeeProcessor;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public ActionResult ViewEmployees()
+        {
+            ViewBag.Message = "Employees List";
+            var data = _employeeProcessor.LoadEmployees();
+            List<EmployeeModel> employees = new List<EmployeeModel>();
+            foreach(var row in data)
+            {
+                employees.Add(new EmployeeModel
+                {
+                    EmployeeId = row.EmployeeId,
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                    ConfirmEmail = row.EmailAddress
+                });
+            }
+            return View(employees);
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(EmployeeModel model)
+        {
+
+            if(ModelState.IsValid)
+            {
+
+                int recordsCreated = _employeeProcessor.CreateEmployee(model.EmployeeId,model.FirstName,
+                    model.LastName, model.EmailAddress);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperWebApp.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name ="Employee ID")]
+        [Range(100000,999999,ErrorMessage ="You need to enter a valid EmployeeId")]
+        public int EmployeeId { get; set;}
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="You need to provide use your first name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "You need to provide use your last name")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Display(Name ="Confirm Email")]
+        [Compare("EmailAddress", ErrorMessage = "The email and confirm email must match")]
+        public string ConfirmEmail { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength =10,ErrorMessage ="You need to provide a long enought password.")]
+        public string Password { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Your password and confirmed password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
